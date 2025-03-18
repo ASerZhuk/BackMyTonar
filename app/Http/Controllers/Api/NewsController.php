1a15a33a3d765e1c0eac42b7ee842a8cdf35a693
ASerZhuk <gudvin109@gmail.com>
2025-03-18 16:51:59 +0300

    5

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -2,25 +2,58 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use App\Models\News;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
     public function index()
-{
-    $news = News::all()->map(function ($item) {
-        return [
-            'id' => $item->id,
-            'title_one' => $item->title_one,
-            'description' => $item->description,
-            'title_two' => $item->title_two,
-            'content' => $item->content,
-            'image_url' => $item->image_url,
-        ];
-    });
+    {
+         $news = News::all()->map(function ($item) {
 
-    return response()->json($news);
-}
+             return [
+                 'id' => $item->id,
+                 'title_one' => $item->title_one,
+                 'description' => $item->description,
+                 'title_two' => $item->title_two,
+                 'content' => $item->content,
+                 'image_url' => $item->image_url,
+                 'date' => $item->created_at = Carbon::parse($item->created_at)->format('d F Y'),
+             ];
+         });
+
+        
+
+        if (!$news) {
+            return response()->json(['message' => 'Not found'], 404);
+        };
+
+        return response()->json($news);
+    }
+
+    public function newsDetail(Request $request) {
+
+        $request->validate([
+            'id' => 'required|integer'
+        ]);
+
+       $news = News::find($request->id);
+            return [
+                'id' => $news->id,
+                'title_one' => $news->title_one,
+                'description' => $news->description,
+                'title_two' => $news->title_two,
+                'content' => $news->content,
+                'image_url' => $news->image_url,
+                'date' => $news->created_at = Carbon::parse($news->created_at)->format('d F Y'),
+            ];
+
+        if (!$news) {
+            return response()->json(['message' => 'Новость не найдена'], 404);
+        }
+
+        return response()->json($news);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\NewsController;
 use App\Http\Controllers\Api\BusNavigationController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 
 Route::get('/bus-navigation', [BusNavigationController::class, 'index']);
 Route::get('/news', [NewsController::class, 'index']);
+Route::post('/news/detail', [NewsController::class, 'newsDetail']);
